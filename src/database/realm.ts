2eb12a347aa7208b20684efd834d08369de15815
MatheusTrinca <matheusfelipetrinca@gmail.com>
2022-12-02 03:52:08 -0300

    add splash

--- src/database/realm.ts
+++ src/database/realm.ts
@@ -1,10 +1,9 @@
 import Realm from 'realm';
 import { CheckListSchema } from './schemas/CheckListSchema';
-import { CheckListSyncSchema } from './schemas/CheckListSyncSchema';
 
 export const getRealm = async () =>
   await Realm.open({
     path: 'milk-hiring',
-    schema: [CheckListSchema, CheckListSyncSchema],
+    schema: [CheckListSchema],
     deleteRealmIfMigrationNeeded: true,
   });
